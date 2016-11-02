1fa57d78e3614eb6576a166c5e955d2ae2ead01a
matsumotory <matsumotory@pepabo.com>
2016-11-02 15:56:19 +0900

    Support TLS parameters

--- handler/example_helo.rb
+++ handler/example_helo.rb
@@ -1,1 +1,7 @@
 puts "helo hostname: #{Pmilter::Session.new.helo_hostname}"
+puts "tls client issuer: #{Pmilter::Session.new.cert_issuer}"
+puts "tls client subject: #{Pmilter::Session.new.cert_subject}"
+puts "tls session key size: #{Pmilter::Session.new.cipher_bits}"
+puts "tls encrypt method: #{Pmilter::Session.new.cipher}"
+puts "tls version: #{Pmilter::Session.new.tls_version}"
+
